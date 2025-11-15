415f0c7ee61bb878891754d902454388b7600573
Flow EdTech <flow@edtech.app>
2025-11-14 21:07:22 -0600

    Fix: Remove back button from profile tab in dashboard to prevent auto-redirect

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1024391166"
+    serviceWorkerVersion: "3217551912"
   }
 });
