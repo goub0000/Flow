3facad725b5331e06d354c022bd8e46b95db08a9
Flow EdTech <flow@edtech.app>
2025-11-27 20:54:25 -0600

    Fix: Rebuild with correct Supabase anon key

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e08561b35f2eedbaa892846a9eb8bd29",
+"flutter_bootstrap.js": "87d2af39ebdc2152c80318d390a19b7d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "dfcec3e22323aa0a5e089e4634ec6a2f",
 "/": "dfcec3e22323aa0a5e089e4634ec6a2f",
-"main.dart.js": "df1c46d14540a7a1bdccd13f2bf8d668",
+"main.dart.js": "674337148296ebe3d9bbc4f48d9548bb",
 "manifest.json": "7ad511958f18df3d13e2eac5cc802b98",
 "version.json": "5482af18966c9a820d64411bad9b0c9e"};
 // The application shell files that are downloaded before a service worker can
