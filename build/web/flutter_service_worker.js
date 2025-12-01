cfe0c86cdc39f70696c2ee3e70745e40d7513ff6
Flow EdTech <flow@edtech.app>
2025-11-30 22:29:59 -0600

    Fix PaginatedResponse to support conversations/messages field names

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "6e2b8b1aefc79c6b085d70c16184d643",
+"flutter_bootstrap.js": "373415b0f7f4b2420c62ebd6fcac582f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "dfcec3e22323aa0a5e089e4634ec6a2f",
 "/": "dfcec3e22323aa0a5e089e4634ec6a2f",
-"main.dart.js": "569aa196b8d0007ab0d32088dd3b3e4c",
+"main.dart.js": "1689b498bfee7b3022c5545f5b06858f",
 "manifest.json": "7ad511958f18df3d13e2eac5cc802b98",
 "version.json": "7ced450e93ccea7dbe4fc30e96d24f07"};
 // The application shell files that are downloaded before a service worker can
