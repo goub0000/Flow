6d46ecc09f194794a26652672db4890e25add8cc
Flow EdTech <flow@edtech.app>
2026-01-21 15:45:43 -0600

    test: Test MouseFollowingGradient

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a8df1a16ec904e1496af4009bd482f84
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "225b9012f433c79ed36bf45f6f3bf2c0",
+"flutter_bootstrap.js": "824cd4dee2a5be22f9e0c738ee943b51",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "dfcec3e22323aa0a5e089e4634ec6a2f",
 "/": "dfcec3e22323aa0a5e089e4634ec6a2f",
-"main.dart.js": "edad6fade25915ebc2d4b7ec869da4f5",
+"main.dart.js": "21be109232e70db3b5b6396ca25fdd0d",
 "manifest.json": "7ad511958f18df3d13e2eac5cc802b98",
 "version.json": "4c0336370a5e555b1e5f75958fba2a75"};
 // The application shell files that are downloaded before a service worker can
